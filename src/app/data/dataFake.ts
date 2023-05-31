2344aaa4e0710d55ea7c8d9427ed073a7778cb95
Dyego Medeiros <dyego.medeiros@car10.com.br>
2023-05-31 17:24:07 -0300

    conteudo dinamico

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+    {
+        "id":'1',
+        "photo":"https://moewalls.com/wp-content/uploads/2023/02/ryu-hadouken-street-fighter-thumb.jpg",
+        "title":"Hadouken!!!",
+        "description":"Ryu vence o torneio Street Fighter. Ele é o novo rei dos lutadores de rua."
+    },
+    {
+        "id":'2',
+        "photo":"https://media.alvanista.com/uploads/timeline_image/2017/07/21/453606_2545907250.jpg",
+        "title":"Pronta para a revanche!!!",
+        "description":"Após derrota humilhante para Cammy, Chun-li passa por um rigoroso treinamento e está pronta para a revanche!"
+    },
+    {
+        "id":'3',
+        "photo":"https://www.streetfighter.com/6/assets/images/character/blanka/blanka_ss05.jpg",
+        "title":"Blanka desabafa no programa da Ana Maria Braga",
+        "description":"Na última terça feira dia 13, Blanka esteve no programa da Ana Maria Braga e confirma: Há preconceito por parte dos outros lutadores com ele, não chegou a citar nomes mas certamente se referia a Vega e Balrog"
+    },
+    {
+        "id":'4',
+        "photo":"https://ih1.redbubble.net/image.1185814879.9600/flat,750x1000,075,f.jpg",
+        "title":"Vega revela segredos dos cabelos sebosos",
+        "description":"Depois de ser chamado para ser o novo garoto propaganda da Loreal Espanha, Vega monta novo canal no youtube onde conta os segredos das madeixas"
+    },
+    {
+        "id":'5',
+        "photo":"https://seriemaniacos.tv/wp-content/uploads/2014/04/Street-Fighter-Assassins-Fist-capa-1068x468.jpg",
+        "title":"O torneio começou!!!",
+        "description":"Começa a nova temporada do Street Fighter! Trazendo novos e antigos lutadores de volta as ruas!!!"
+    },
+]
